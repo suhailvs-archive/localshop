36c742cce78c54b5672d96a8d08426f3831a0db9
suhailvs <suhailvs@gmail.com>
2025-05-02 11:47:47 +0530

    added user tabs

---
+++ app/(app)/(tabs)/wants.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const UserWants = () => {
+  return (
+    <View>
+      <Text>UserWants</Text>
+    </View>
+  )
+}
+
+export default UserWants
